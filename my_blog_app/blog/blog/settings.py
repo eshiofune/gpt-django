44307b818b9cc17d4b68fc44fb22f9e72997781a
Evans Ehiorobo <ehioroboevans@gmail.com>
2024-04-05 17:31:28 +0100

    Remove unsafe key

--- my_blog_app/blog/blog/settings.py
+++ my_blog_app/blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "0*tq*ycuuq0@=ah8u%qavtpn@ph&v0(@z_7n8q0%ff%u8y6lw1"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
